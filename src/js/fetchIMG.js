4f0e642b623e0fccb561bef3015daaf8aaa9f1f3
Taras Garan'ko <garanko.t@gmail.com>
2021-11-29 18:00:47 +0200

    1

--- src/js/fetchIMG.js
+++ src/js/fetchIMG.js
@@ -12,12 +12,12 @@ async function getUser() {
     try {
       const response = await axios.get('/user?ID=12345');
       console.log(response);
-      if (!response.ok) {
-        if (response.status === 404) {
-            Notify.failure('Oops, there is no country with that name');
-        }
-        throw new Error(response.statusText);
-    }
+    //   if (!response.ok) {
+    //     if (response.status === 404) {
+    //         Notify.failure('Oops, there is no country with that name');
+    //     }
+    //     throw new Error(response.statusText);
+    // }
     console.log(response.json())
     return await response.json();
     } catch (error) {
